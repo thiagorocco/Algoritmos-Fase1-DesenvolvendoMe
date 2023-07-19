74d325dd1f5bdd832faba1a88f3a78351718f434
thiagorocco <deroccotr@gmail.com>
2023-07-19 07:23:06 -0300

    atualizações de comentários arquivo 02-counting_sort_ordenação_contagem.py

--- 02-SortingAlgorithms_Algoritmos_de_ordenação/02-counting_sort_ordenação_contagem.py
+++ 02-SortingAlgorithms_Algoritmos_de_ordenação/02-counting_sort_ordenação_contagem.py
@@ -12,13 +12,17 @@ def ordenacaoContagem(arr):
     count = [0 for i in range(256)]
 
     # Para armazenar o resultado resultante, pois a string é variável
+    #Nesse caso "ans" terá o mesmo número de elementos de "arr"
+    #e iniciará todos eles com vazio("")  
     ans = ["" for _ in arr]
 
-    # Armazena a contagem de cada caractere
+    #Varra o array "arr" pegando cada elemento dele com i
+    #A cada interação uma posição de count será incrementada, essa posição será com base
+    #no código ascii do valor de i. Pois um array pode conter números, letras e caracteres
     for i in arr:
         count[ord(i)] += 1
 
-    #Mude count[i] para que count[i] agora contenha a posição atual desse caracterer no array de saída
+    #Mude count[i] para que count[i] agora tenha a posição atual desse caracterer no array de saída
     for i in range(256):
         count[i] += count[i-1]
 
