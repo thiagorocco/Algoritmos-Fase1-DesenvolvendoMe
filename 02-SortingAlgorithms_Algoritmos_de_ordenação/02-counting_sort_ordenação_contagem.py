69069e475c66a4206ae18c7e4f3e77e19b188941
thiagorocco <deroccotr@gmail.com>
2023-07-19 20:53:32 -0300

    Explicação do algoritmo de contagem esmiuçado antes do código

--- 02-SortingAlgorithms_Algoritmos_de_ordenação/02-counting_sort_ordenação_contagem.py
+++ 02-SortingAlgorithms_Algoritmos_de_ordenação/02-counting_sort_ordenação_contagem.py
@@ -1,6 +1,23 @@
 # Programa para ordenação por contagem
 
 #A função principal que ordenar deixará a string arr in ordem alfabética
+'''
+    Passos para ordenar por contagem
+    1. Obtenha o tamanho do vetor desordenado
+    2. Crie um vetor de contagem de 0 a 9 para elementos números ou 0 a 255 para caracteres
+    3. Inicialize o vetor de contagem com todos os elementos em 0 ou vazio
+    4. Percorra cada elemento do array desordenado, ao encontrar o elemento incremente
+    a posição correspondente ao seu valor em +1. Ex: Achou 2 incremente a posição 2 com +1
+    5. Agora esse array de contagem precisa ter cada um dos seus termos somados com a 
+    posição anterior respectivamente. Ex: 0,2,2,0 -> 0,2,4,4
+    6. Crie um novo array de resultado com o mesmo número de elementos do array desordenado
+    7. Percorra cada elemento do array desordenado de trás para frente. A cada elemento
+    checado verifique qual o valor está associado no array de contagem.
+    Ex: Elemento 2 do array desordenado possui no array de contagem o valor 4.
+    Dessa forma o valor 2 será inserido na posição 4 do array resultado.
+    8. Depois de inserir o valor no array resultado é preciso decrementar em -1
+    o valor associado no array de contagem, então se na posição 2 tinha-se 4, agora terá 3
+'''
 
 
 def ordenacaoContagem(arr):
