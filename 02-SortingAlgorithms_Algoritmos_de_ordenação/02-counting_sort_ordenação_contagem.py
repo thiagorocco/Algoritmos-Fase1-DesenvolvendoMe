f7b50063920fa556acc9fa4c1054b358b72b6408
thiagorocco <deroccotr@gmail.com>
2023-07-20 07:09:47 -0300

    Algoritmo de ordenação por contagem compreendido, falta implementar uma forma de ordenar tanto array de string como númerico

--- 02-SortingAlgorithms_Algoritmos_de_ordenação/02-counting_sort_ordenação_contagem.py
+++ 02-SortingAlgorithms_Algoritmos_de_ordenação/02-counting_sort_ordenação_contagem.py
@@ -24,38 +24,35 @@
 def ordenacaoContagem(arr):
 
     # o array de saída(output) que terá arr ordenado
-    output = [0 for i in range(len(arr))]
+    #output = [0 for i in range(len(arr))]
+    output = ["" for _ in arr]
 
     # Crie um array contador que guarde a contagem dos caracteres individuais e inicialize a contagem do array como O
     count = [0 for i in range(256)]
 
-    # Para armazenar o resultado resultante, pois a string é variável
-    #Nesse caso "ans" terá o mesmo número de elementos de "arr"
-    #e iniciará todos eles com vazio("")  
-    ans = ["" for _ in arr]
-
     #Varra o array "arr" pegando cada elemento dele com i
     #A cada interação uma posição de count será incrementada, essa posição será com base
     #no código ascii do valor de i. Pois um array pode conter números, letras e caracteres
+    # 
     for i in arr:
-        count[ord(i)] += 1
-
-    #Mude count[i] para que count[i] agora tenha a posição atual desse caracterer no array de saída
+        try:
+            int(i)
+            count[i] += 1
+        except:
+            count[ord(i)] += 1
+    #5. Soma do elemento atual com o anterior no array de contagem
     for i in range(256):
         count[i] += count[i-1]
 
-    #Construa o array de caracteres de saída
+    #A cada interação 
     for i in range(len(arr)):
         output[count[ord(arr[i])]-1] = arr[i]
         count[ord(arr[i])] -= 1
+   
+    return output
 
-    # Copie o array de saída para arr, 
-    # de modo que arr agora contenha os caracteres ordenados
-    for i in range(len(arr)):
-        ans[i] = output[i]
-    return ans
-
-arr = "abacate"
+#arr = "banana123"
+arr = [8,3,1,4,12]
 ans = ordenacaoContagem(arr)
 merge_chars = "".join(ans)
 
